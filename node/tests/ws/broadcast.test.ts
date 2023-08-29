a0a5ef89778778c3e273bea11668f6a11af08fcf
颖逸 <49649786+Zuoqiu-Yingyi@users.noreply.github.com>
2023-08-29 16:50:59 +0800

    feat(api): 添加 API `/ws/broadcast` | Add API `/ws/broadcast`

---
+++ node/tests/ws/broadcast.test.ts
@@ -0,0 +1,55 @@
+/**
+ * Copyright (C) 2023 SiYuan Community
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import {
+    describe,
+    test,
+    expect,
+} from "vitest";
+import CONSTANTS from "~/tests/constants";
+
+import client from "~/tests/utils/client";
+
+const pathname = client.Client.ws.broadcast.pathname;
+
+describe(pathname, async () => {
+
+    const message = new Date().toString();
+
+    test("test channel push and listen message", async () => {
+        const ws = client.client.broadcast({
+            channel: CONSTANTS.BROADCAST_CHANNEL_NAME,
+        });
+        await expect.soft(
+            new Promise((resolve, rejects) => {
+                const lestener = (e: WebSocketEventMap["message"]) => {
+                    client.broadcast.removeEventListener("message", lestener);
+                    resolve(e.data);
+                }
+
+                client.broadcast.addEventListener("message", lestener);
+
+                ws.addEventListener("error", rejects);
+                ws.addEventListener("open", () => {
+                    ws.send(message);
+                });
+            }),
+            "listen message",
+        ).resolves.toEqual(message);
+        ws.close();
+    });
+});
